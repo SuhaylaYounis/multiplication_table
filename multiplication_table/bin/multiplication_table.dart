64a6d07bf8e518c64e1957d9d1b325122e4782ee
SuhaylaYounis <60473969+SuhaylaYounis@users.noreply.github.com>
2021-07-27 14:10:31 +0200

    Dart program to print first 10 multiplication tables
    Dart program to print first 10 multiplication tables using for loops.

---
+++ multiplication_table/bin/multiplication_table.dart
@@ -0,0 +1,12 @@
+import "dart:io";
+void main(List<String> arguments) 
+{
+  for(int i=0;i<=10;i++)
+    {
+      for(int n=1;n<=10;n++)
+        {
+          print(i*n);
+        }
+      print("-----------------");
+    }
+}
